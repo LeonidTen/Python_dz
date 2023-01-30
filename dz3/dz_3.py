d54d5ad3c32359e7cb31e1fee79f0d1db3829f76
leonid <tenleonid@gmail.com>
2023-01-30 13:24:24 +0600

    dz3

---
+++ dz3/dz_3.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+# в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+def input_coordinates():
+    x, y = map(int, input("Введите X, Y через пробел: ").split())
+    return x, y
+
+x, y = input_coordinates()
+
+def checkCoordinates(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+    else:
+        return "Введены координаты 0, 0"
+
+f = checkCoordinates(x, y)
+print(f)
